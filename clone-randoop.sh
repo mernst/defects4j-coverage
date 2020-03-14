7915f4ab1ad80bd8ef15136aa83a5c0e99812549
Michael Ernst <mernst@cs.washington.edu>
2020-03-14 11:17:11 -0700

    More focused testing

--- clone-randoop.sh
+++ clone-randoop.sh
@@ -13,4 +13,3 @@ fi
 /tmp/plume-scripts/git-clone-related randoop randoop /tmp/randoop
 # Print Randoop version number
 (LIBS=/tmp/randoop/build/libs/ && cd /tmp/randoop && rm -rf ${LIBS} && ./gradlew assemble && java -classpath ${LIBS}/randoop-all-*.jar randoop.main.Main help | grep version)
-
